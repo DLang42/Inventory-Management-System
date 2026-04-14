671180d00022d37d5b84ab97502a210a6b3f3383
dhlang <dhlang@ncsu.edu>
2026-04-13 21:15:44 -0400

    Fully implemented purchase inventory feature

---
+++ Inventory-Management-System/ViewModels/PurchaseViewModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Inventory_Management_System.ViewModels;
+
+public class PurchaseViewModel
+{
+    [Required] public string PONumber { get; set; } = string.Empty;
+    [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "You have to select an inventory.")]
+    public int InventoryId { get; set; }
+    [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quantity has to be greater than or equal to 1.")]
+    public int QuantityToPurchase { get; set; }
+    public double InventoryPrice { get; set; }
+}
